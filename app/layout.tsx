00910e5aba0f9f60a31c1a2026c30b770e90512f
Nicolas <nicnan12345@gmail.com>
2025-04-18 23:04:24 -0300

    se agrego archivos para provar Html y css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,15 +25,45 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+      <link rel="icon" type="image/png" href="/logo.png"  />
+      </Head>
       <body>
         {/* Navbar */}
-        <div className="navbar fixed z-[100] bg-base-100 shadow-sm">
-          <div className="flex-1">
+        <div className="navbar fixed z-[100] bg-base-100 shadow-sm px-8">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                <li><a>Item 1</a></li>
+                <li>
+                  <a>Parent</a>
+                  <ul className="p-2">
+                    <li><a>Submenu 1</a></li>
+                    <li><a>Submenu 2</a></li>
+                  </ul>
+                </li>
+                <li><a>Item 3</a></li>
+              </ul>
+            </div>
             <a className="btn btn-ghost text-xl">NicoShop</a>
           </div>
+          <div className="navbar-end hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+              <li><a href="/contact">Contactos</a></li>
+            </ul>
+          </div>
+          
+          {/* <div className="flex-1">
+            <a className="btn btn-ghost text-xl">NicoShop</a>
+          </div> */}
         </div>
         {children}
-        <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content items-center p-4 mt-30">
+        <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content items-center p-4 mt-30 px-8">
           <aside className="grid-flow-col items-center">
             <svg
               width="36"
